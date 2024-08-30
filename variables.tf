d94f6ea269f98f564f24e56b39f8f014fdcef249
Josphat Mutai <josphatkmutai@gmail.com>
2024-08-30 12:52:06 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "nginx_ingress" {
+  description = "Variables set for deployment of Nginx Ingress Controller."
+  type = object({
+    namespace          = string
+    replicas           = number
+    ingress_class_name = string
+    chart_repository   = string
+    chart_name         = string
+    chart_version      = string
+  })
+  default = {
+    namespace          = "nginx-ingress"
+    replicas           = 1
+    ingress_class_name = "nginx"
+    chart_repository   = "https://kubernetes.github.io/ingress-nginx"
+    chart_name         = "ingress-nginx"
+    chart_version      = "4.10.3"
+  }
+}
